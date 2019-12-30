ef0dc16cb0e877fe2badf0eb69cbb509bae271f2
wilfredlams <wilfrednetwork@gmail.com>
2019-12-30 12:46:39 +1100

    Permission and background changes

--- app/src/main/java/com/androiddeft/decentralcabs/DashboardActivity.java
+++ app/src/main/java/com/androiddeft/decentralcabs/DashboardActivity.java
@@ -65,15 +65,12 @@ public class DashboardActivity extends AppCompatActivity {
     private static final String KEY_EMPTY = "";
     private String updateloc_url = "https://yodonga.com/decab/updateloc.php";
     private String listlatlong_url = "https://yodonga.com/decab/listlatlong.php";
-    private ProgressDialog pDialog;
-    private Handler refreshlatlongloop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestwritepermission();
-        requestlocationpermission();
+        requestpermissions();
 
         int SDK_INT = android.os.Build.VERSION.SDK_INT;
         if (SDK_INT > 8)
@@ -273,53 +270,27 @@ public class DashboardActivity extends AppCompatActivity {
 
 
 
-    public void requestwritepermission(){
+    public void requestpermissions(){
         // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this,
+        if ((ContextCompat.checkSelfPermission(this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-            // Permission has already been granted
-        }
-
-
-    }
-
-    public void requestlocationpermission(){
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this,
+                != PackageManager.PERMISSION_GRANTED) && (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+                != PackageManager.PERMISSION_GRANTED)){
+
 
             // Permission is not granted
             // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+            if ((ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) && (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION))) {
                 // Show an explanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
                 // sees the explanation, try again to request the permission.
             } else {
                 // No explanation needed; request the permission
                 ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION},
                         MY_PERMISSIONS_REQUEST_READ_CONTACTS);
 
                 // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
@@ -329,6 +300,7 @@ public class DashboardActivity extends AppCompatActivity {
         } else {
             // Permission has already been granted
         }
+
 
     }
 
